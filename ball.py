58cade3873603a045c901d4ca9b95e7dec5748e5
Diego Rojas <diegorojas431@gmail.com>
2022-06-17 20:36:38 -0500

    reconocimiento de color

--- ball.py
+++ ball.py
@@ -5,6 +5,5 @@ def main():
     valor = pared2.salto()
     print("Terminando")
 
-
 if __name__ == "__main__":
     main()
--- pared2.py
+++ pared2.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as np
 
+camara = cv2.VideoCapture(0)
+
 def salto():
-  camara = cv2.VideoCapture(0)
 
-  redBajo1 = np.array([[[0, 100, 20]]],np.unit8)
-  redAlto1 = np.array([[[8, 255, 255]]],np.unit8)
+  redBajo1 = np.array([0, 100, 20],np.uint8)
+  redAlto1 = np.array([8, 255, 255],np.uint8)
 
-  redBajo2 = np.array([[[175, 100, 20]]],np.unit8)
-  redAlto2 = np.array([[[179, 255, 255]]],np.unit8)
+  redBajo2 = np.array([175, 100, 20],np.uint8)
+  redAlto2 = np.array([179, 255, 255],np.uint8)
 
   while True:
     ret, frame = camara.read()
@@ -16,8 +17,11 @@ def salto():
       frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
       maskRed1 = cv2.inRange(frameHSV, redBajo1, redAlto1)
       maskRed2 = cv2.inRange(frameHSV, redBajo2, redAlto2)
-      maskRed = cv2.inRange(frameHSV, redBajo2, redAlto2)
+      maskRed = cv2.add(maskRed1, maskRed2)
 
+      maskRedvis = cv2.bitwise_and(frame, frame, mask = maskRed)
+
+      cv2.imshow("maskRedvis", maskRedvis)
       cv2.imshow("maskRed", maskRed)
       cv2.imshow("frame", frame)
       if cv2.waitKey(30) & 0xFF == ord('q'):
